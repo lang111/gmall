9177fba1902a8df640b0b2d9af8926ae189e12b7
lang111 <53170210+lang111@users.noreply.github.com>
2019-11-11 22:49:11 +0800

    Update BaseAttrInfo.java

--- gmall-bean/src/main/java/com/atguigu/gmall/bean/BaseAttrInfo.java
+++ gmall-bean/src/main/java/com/atguigu/gmall/bean/BaseAttrInfo.java
@@ -16,7 +16,7 @@ public class BaseAttrInfo implements Serializable {
     private String attrName;
     @Column
     private String catalog3Id;
-    //添加信息啊
+    //添加
     @Transient
     private List<BaseAttrValue> attrValueList;
 
